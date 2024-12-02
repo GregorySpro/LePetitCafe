45a96c0282c0dc798b206a81d4ae8a38e42352b5
Grégory <146572387+GregorySpro@users.noreply.github.com>
2024-12-02 10:15:40 +0100

    upload files

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+require_once 'includes/header.php';
+require_once 'includes/navbar.php';
+require_once 'includes/hero.php';
+require_once 'includes/menu.php';
+require_once 'includes/ambiance.php';
+require_once 'includes/contact.php';
+require_once 'includes/footer.php';
+?>
